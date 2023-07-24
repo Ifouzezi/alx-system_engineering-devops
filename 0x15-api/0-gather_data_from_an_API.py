8cc5eba03e0b02e83a58417ad671e8c3430062d3
ifouzezi <ifoghaleuzezi@gmail.com>
2023-07-24 14:10:38 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,10 +17,13 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             employee_id = int(sys.argv[1])
-            user_response = requests.get('{}/users/{}'.format(API_ENDPOINT, employee_id)).json()
-            todos_response = requests.get('{}/todos'.format(API_ENDPOINT)).json()
+            user_response = requests.get(
+                '{}/users/{}'.format(API_ENDPOINT, employee_id)).json()
+            todos_response = requests.get(
+                '{}/todos'.format(API_ENDPOINT)).json()
             employee_name = user_response.get('name')
-            todos = list(filter(lambda x: x.get('userId') == employee_id, todos_response))
+            todos = list(filter
+                         (lambda x: x.get('userId') == employee_id, todos_response))
             todos_done = list(filter(lambda x: x.get('completed'), todos))
             print(
                 'Employee {} is done with tasks({}/{}):'.format(

bc3889fc3dbf1475b3c1ebfbd2cadf59e81576ef
ifouzezi <ifoghaleuzezi@gmail.com>
2023-07-24 12:48:13 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
+""" Queries REST API for employee info, exports to CSV
+    "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    argv 1 = int employee ID
 """
-Using https://jsonplaceholder.typicode.com
-gathers data from API and exports it to CSV file
-Implemented using recursion
-"""
-import re
-import requests
-import sys
+if __name__ == "__main__":
+    import csv
+    import requests as r
+    from sys import argv
 
+    user_id = argv[1]
+    # Finds employee username by "id" param in /users/
+    name_q = r.get("https://jsonplaceholder.typicode.com/users/{}/"
+                   .format(user_id))
+    data = name_q.json()
+    username = data.get("username")
 
-API_ENDPOINT = "https://jsonplaceholder.typicode.com"
-"""REST API URL"""
+    # Finds employee tasks by "userID" param; /users/ & /todo/ are linked
+    url = "https://jsonplaceholder.typicode.com/users/1/todos/"
+    task_q = r.get(url, params={'userId': user_id})
+    data = task_q.json()
 
+    # Creates csv file, writes request data into it row by row
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            employee_id = int(sys.argv[1])
-            user_response = requests.get('{}/users/{}'.format(API_ENDPOINT, employee_id)).json()
-            todos_response = requests.get('{}/todos'.format(API_ENDPOINT)).json()
-            employee_username = user_response.get('username')
-            todos_list = list(filter(lambda x: x.get('userId') == employee_id, todos_response))
-            with open('{}.csv'.format(employee_id), 'w') as file:
-                for todo in todos_list:
-                    file.write(
-                        '"{}","{}","{}","{}"\n'.format(
-                            employee_id,
-                            employee_username,
-                            todo.get('completed'),
-                            todo.get('title')
-                        )
-                    )
+    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
+        csv_data = csv.writer(csvfile, delimiter=',', quotechar='"',
+                              quoting=csv.QUOTE_ALL)
+        for elem in data:
+            csv_data.writerow([elem["userId"], username,
+                               elem["completed"], elem["title"]])

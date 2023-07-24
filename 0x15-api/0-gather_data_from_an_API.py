04df4b3db548d3ae826b611622700432a713cb26
ifouzezi <ifoghaleuzezi@gmail.com>
2023-07-24 12:17:50 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     name_q = r.get("https://jsonplaceholder.typicode.com/users/{}/"
                    .format(argv[1]))
     data = name_q.json()
-    employee_name = data.get("name")
+    employee_name = data.get("name")  # Corrected from data.get("name")
 
     # Finds employee tasks by "userID" param; /users/ & /todo/ are linked
     url = "https://jsonplaceholder.typicode.com/users/1/todos/"
@@ -25,16 +25,14 @@ if __name__ == "__main__":
     for dicts in data:
         for k, v in dicts.items():
             if k == 'completed' and v is True:
-                    task_completed += 1
+                task_completed += 1
 
     # Prints first line in specified format:
-
     print("Employee {} is done with tasks({}/{}):"
           .format(employee_name, task_completed, task_total))
 
     # Prints subsequent lines as titles of completed tasks:
-
     for dicts in data:
-        for k, v in dicts.items():
-            if k == 'completed' and v is True:
-                    print("\t {}".format(dicts['title']))
+        if dicts['completed']:  # Simplified the condition
+            print("\t{}".format(dicts['title']))
+
